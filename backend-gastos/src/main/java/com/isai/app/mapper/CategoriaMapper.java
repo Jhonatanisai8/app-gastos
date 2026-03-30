1ce32dd7f303db581aa6d7490397d140ce56ea65
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-30 01:28:25 -0500

    Add CategoriaMapper interface for mapping Categoria entity to CategoriaResponse DTO

---
+++ backend-gastos/src/main/java/com/isai/app/mapper/CategoriaMapper.java
@@ -0,0 +1,11 @@
+package com.isai.app.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.isai.app.models.dtos.CategoriaResponse;
+import com.isai.app.models.entities.Categoria;
+
+@Mapper(componentModel = "spring")
+public interface CategoriaMapper {
+    CategoriaResponse toCategoriaResponse(Categoria categoria);
+}
