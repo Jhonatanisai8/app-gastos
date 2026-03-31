7c395728a6cec6f950c3295c84c12ec6974db3eb
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-30 20:40:31 -0500

    Add CategoriaRepository interface for CRUD operations on Categoria entity

---
+++ backend-gastos/src/main/java/com/isai/app/repository/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.isai.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.isai.app.models.entities.Categoria;
+
+@Repository
+public interface CategoriaRepository
+        extends CrudRepository<Categoria, Long> {
+
+}
