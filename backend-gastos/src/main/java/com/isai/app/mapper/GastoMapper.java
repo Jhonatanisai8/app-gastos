8431abc508c50d2010d3bedb09424b8cd64b06ef
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-31 21:49:39 -0500

    feat: implement GastoMapper interface to convert Gasto entities to GastoResponse DTOs

--- backend-gastos/src/main/java/com/isai/app/mapper/GastoMapper.java
+++ backend-gastos/src/main/java/com/isai/app/mapper/GastoMapper.java
@@ -2,6 +2,10 @@ package com.isai.app.mapper;
 
 import org.mapstruct.Mapper;
 
+import com.isai.app.models.dtos.res.GastoResponse;
+import com.isai.app.models.entities.Gasto;
+
 @Mapper(componentModel = "spring")
 public interface GastoMapper {
-    }
+    GastoResponse toGastoResponse(Gasto gasto);
+}
