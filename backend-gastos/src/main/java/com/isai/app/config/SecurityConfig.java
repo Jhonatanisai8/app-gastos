675ebdb3f94d2b7e18897b04888d595873fc1a44
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-04-02 16:17:16 -0500

    feat: add application configuration for authentication and user details service

--- backend-gastos/src/main/java/com/isai/app/config/SecurityConfig.java
+++ backend-gastos/src/main/java/com/isai/app/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package com.isai.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.isai.app.jwt.JwtAutehnticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,13 +18,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAutehnticationFilter autehnticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
         return httpSecurity
                 .csrf(crsf -> crsf.disable())
                 .authorizeHttpRequests(authRequest -> authRequest.requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
+                .sessionManagement(sesion -> sesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(autehnticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
 }
