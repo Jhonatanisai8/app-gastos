cc91f4537772290dfa97e3be38bafb33bfe49f41
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-04-02 22:25:07 -0500

    feat: quitando importacion innesarias

--- backend-gastos/src/main/java/com/isai/app/jwt/JwtAutehnticationFilter.java
+++ backend-gastos/src/main/java/com/isai/app/jwt/JwtAutehnticationFilter.java
@@ -7,7 +7,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -25,39 +24,39 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JwtAutehnticationFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService;
-    private final UserDetailsService detailsService;
+  private final JwtService jwtService;
+  private final UserDetailsService detailsService;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        final String token = obtenerTokenDelRequest(request);
-        final String userName;
-        if (token == null) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        userName = jwtService.obtenerNombreUsuario(token);
-        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = detailsService.loadUserByUsername(userName);
-            // validamos que el token sea valido
-            if (jwtService.esTokenValido(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                        userName, null, userDetails.getAuthorities());
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource()
-                        .buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            }
-        }
-        filterChain.doFilter(request, response);
-
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    final String token = obtenerTokenDelRequest(request);
+    final String userName;
+    if (token == null) {
+      filterChain.doFilter(request, response);
+      return;
     }
-
-    private String obtenerTokenDelRequest(HttpServletRequest request) {
-        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
-            return authHeader.substring(7);
-        }
-        return null;
+    userName = jwtService.obtenerNombreUsuario(token);
+    if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = detailsService.loadUserByUsername(userName);
+      // validamos que el token sea valido
+      if (jwtService.esTokenValido(token, userDetails)) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+            userName, null, userDetails.getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource()
+            .buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+      }
     }
+    filterChain.doFilter(request, response);
+
+  }
+
+  private String obtenerTokenDelRequest(HttpServletRequest request) {
+    final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+    if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
+      return authHeader.substring(7);
+    }
+    return null;
+  }
 }
